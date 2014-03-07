6c722b25752aed7825a18f930520d931a938b553
João Lopes <joaopclopes@gmail.com>
2014-03-07 16:10:17 +0000

    Added all main clases and comments.

---
+++ src/up/fe/liacc/repacl/IRepACLAgent.java
@@ -0,0 +1,5 @@
+package up.fe.liacc.repacl;
+
+public class IRepACLAgent {
+
+}
---
+++ src/up/fe/liacc/repacl/junit/ClassTests.java
@@ -0,0 +1,15 @@
+package up.fe.liacc.repacl.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ClassTests {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
