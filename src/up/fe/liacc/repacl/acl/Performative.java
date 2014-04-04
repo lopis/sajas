d6fb6f383c3024139742b37bb12354d7a2a175cb
João Lopes <joaopclopes@gmail.com>
2014-04-04 09:27:50 +0100

    Classes Performative and Protocol now contain constants
    Instead of them being all inside the ACL class. ACL class was deleted.

--- src/up/fe/liacc/repacl/acl/Performative.java
+++ src/up/fe/liacc/repacl/acl/Performative.java
@@ -1,14 +1,6 @@
 package up.fe.liacc.repacl.acl;
 
-public class ACL {
-	
-	/* Protocols */
-	
-	final static public int FIPA_REQUEST = 1;
-	final static public int FIPA_CONTRACT_NET = 2;
-	final static public int FIPA_PROPOSE = 3;
-	final static public int NO_PROTOCOL = -1;
-
+public class Performative {
 	
 	/* Performatives */
 	
---
+++ src/up/fe/liacc/repacl/acl/Protocol.java
@@ -0,0 +1,12 @@
+package up.fe.liacc.repacl.acl;
+
+public class Protocol {
+
+	
+	/* Protocols */
+	
+	final static public int FIPA_REQUEST = 1;
+	final static public int FIPA_CONTRACT_NET = 2;
+	final static public int FIPA_PROPOSE = 3;
+	final static public int NO_PROTOCOL = -1;
+}
