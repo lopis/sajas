f59b9c7f8a3cecc03c72984dabe6eaee291cb447
João Lopes <joaopclopes@gmail.com>
2014-05-12 19:35:02 +0100

    Moved AID to correct package

---
+++ src/up/fe/liacc/sajas/core/AID.java
@@ -0,0 +1,17 @@
+package up.fe.liacc.sajas.core;
+
+public class AID {
+	
+	private String localName;
+	
+	
+	public AID(String localName) {
+		this.localName = localName;
+	}
+
+	public String getLocalName() {
+		return localName;
+	}
+	
+	
+}
